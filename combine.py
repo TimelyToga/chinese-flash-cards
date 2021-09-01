ccf071b903319c42a74805e4e01201c951f450d0
Timothy Blumberg <tblumberg39@gmail.com>
2021-09-01 17:57:53 -0500

    Finalized V1 of combine.py behavior

--- combine.py
+++ combine.py
@@ -5,9 +5,12 @@ flashcard data file: `all.tsv`
 
 import csv
 import os
+from pathlib import Path
+import time
 
 SETS_DIR = 'sets'
 OUTPUT_DIR = 'final'
+OUTPUT_FILE_PREFIX = 'all-terms__'
 
 def get_terms(fn):
     with open(fn) as fd:
@@ -67,29 +70,51 @@ def dedup_terms(term_lst):
             dupes += len(v)
             term_map[k] = merge_definitions(v)
 
+    return term_map, dupes
+
+def render_final_list(term_map):
     # Render definition lists to str
     final_list = []
     for k, def_lst in term_map.items():
         def_lst_str = "; ".join(def_lst)
         final_list.append((k, def_lst_str))
 
-    return term_map, dupes
+    return final_list
 
 def combine(sets_dir, output_dir):
     raw_terms = []
+    file_count = 0
     for set in os.listdir(sets_dir):
         rel_set_path = os.path.join(sets_dir, set)
         print(f'Parsing set: {rel_set_path}')
 
         raw_terms.extend(get_terms(rel_set_path))
+        file_count += 1
 
-    print(f'Master list of {len(raw_terms)} terms')
+    print(f'Parsed {len(raw_terms)} terms')
 
     term_map, dupe_count = dedup_terms(raw_terms)
-    print(f'{len(term_map.keys())} unique terms')
-    print(f'{len(term_map.keys()) + dupe_count} total definitions')
+    print(f'{len(term_map.keys())} unique terms from {file_count} files')
+    print(f'{dupe_count} duplicate definitions merged')
 
-    
+    final_term_list = render_final_list(term_map)
+    write_to_file(final_term_list, output_dir)
+
+def write_to_file(term_list, output_dir):
+    # Ensure the output_dir is created 
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    # Create new file in output_dir with unique name
+    curtime_s_str = str(int(time.time()))
+    output_file_path = os.path.join(output_dir, f'{OUTPUT_FILE_PREFIX}{curtime_s_str}.tsv')
+
+    # Write termlist to file as tsv 
+    with open(output_file_path, 'w') as output_file:
+        w = csv.writer(output_file, delimiter="\t", quotechar='"')
+        for term in term_list:
+            w.writerow(term)
+
+    print(f'\n\nSuccessfully wrote {len(term_list)} flashcards to {output_file_path}.')
 
 
 if __name__ == '__main__': 
